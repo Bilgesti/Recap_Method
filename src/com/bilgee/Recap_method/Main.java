604e3b28592508da698378e6806dc37a1a7fe2c8
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2023-09-28 14:05:53 +0200

    Classes and object

--- src/com/bilgee/Recap_method/Main.java
+++ src/com/bilgee/Recap_method/Main.java
@@ -5,17 +5,20 @@ public class Main {
 
         // Object creation AKA Instantiation
         // När man skapar en objekt alltid börja med klassens namn. Det heter datatype
-        Student benny = new Student();
-        Student frida = new Student();
+        Student benny = new Student(15,"Benny", true);
+        Student frida = new Student(22,"Frida", false);
 
         // Set object Values
-        benny.name = "Benny";
-        frida.name = "Frida";
+
 
         // Print object values
         System.out.println(benny.name);
-        System.out.println(frida.name);
+        System.out.println(benny.age);
+        System.out.println(benny.showIfSleepy());
 
+        System.out.println(frida.name);
+        System.out.println(frida.age);
+        System.out.println(frida.showIfSleepy());
 
 
 
--- src/com/bilgee/Recap_method/Student.java
+++ src/com/bilgee/Recap_method/Student.java
@@ -6,8 +6,24 @@ public class Student {
     public String name;
     public boolean isTired;
 
+    // Constructor
+    public Student(int age, String name, boolean isTired) {
+        this.age = age;
+        this.name = name;
+        this.isTired = isTired;
+    }
+
+
     // Methods
     // TODO - Create methods
+    public String showIfSleepy () {
+        if(isTired== true) {
+           return name + " is very tired";
+        }else {
+            return name + " is NOT tired";
+        }
 
+
+    }
 
 }
