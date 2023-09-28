039733227ddbd36aa6472061d286005365e84947
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2023-09-28 13:48:26 +0200

    Class

--- src/com/bilgee/Recap_method/Main.java
+++ src/com/bilgee/Recap_method/Main.java
@@ -3,6 +3,21 @@ package com.bilgee.Recap_method;
 public class Main {
     public static void main(String[] args) {
 
+        // Object creation AKA Instantiation
+        // När man skapar en objekt alltid börja med klassens namn. Det heter datatype
+        Student benny = new Student();
+        Student frida = new Student();
+
+        // Set object Values
+        benny.name = "Benny";
+        frida.name = "Frida";
+
+        // Print object values
+        System.out.println(benny.name);
+        System.out.println(frida.name);
+
+
+
 
     }
 
---
+++ src/com/bilgee/Recap_method/Student.java
@@ -0,0 +1,13 @@
+package com.bilgee.Recap_method;
+
+public class Student {
+    // Variables
+    public int age;
+    public String name;
+    public boolean isTired;
+
+    // Methods
+    // TODO - Create methods
+
+
+}
